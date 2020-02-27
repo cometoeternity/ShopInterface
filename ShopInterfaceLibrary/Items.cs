88236838c1eaae522c5eddd0d579b1f1f6a4107d
dotoeternity@outlook.com <dotoeternity@outlook.com>
2020-02-27 19:34:34 +0700

    Добавление библиотеки для выполнения логики.

---
+++ ShopInterfaceLibrary/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopInterfaceLibrary
+{
+    public class Items
+    {
+        public string Title { get; set; }
+        public string Discription { get; set; }
+        public decimal Price { get; set; }
+        public bool Sold { get; set; }
+        public bool PaymentDistributed { get; set; }
+        public Vendor Owner { get; set; }
+    }
+}
---
+++ ShopInterfaceLibrary/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopInterfaceLibrary
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public List<Vendor> Vendors { get; set; }
+        public List<Items> Items  { get; set; }
+    }
+}
